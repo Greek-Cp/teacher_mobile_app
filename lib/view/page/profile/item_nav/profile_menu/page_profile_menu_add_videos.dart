2b6c707780ace9c613127e2ca7b5a303e36fc2a0
Greek-Cp <82995911+Greek-Cp@users.noreply.github.com>
2023-08-18 15:29:02 +0700

    Add video fixed

--- lib/view/page/profile/item_nav/profile_menu/page_profile_menu_add_videos.dart
+++ lib/view/page/profile/item_nav/profile_menu/page_profile_menu_add_videos.dart
@@ -526,150 +526,15 @@ class _RowVideoState extends State<RowVideo> {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      crossAxisAlignment: CrossAxisAlignment.center,
-      mainAxisSize: MainAxisSize.max,
-      children: [
-        CardVideo(
-          hintText: "Tap to select a video",
-          labelVideo: "Video",
-          getVideoPath: (pathVideo) {
-            setState(() {
-              videoPath = pathVideo;
-            });
-          },
-        ),
-        CardImageThumbnail(
-          hintText: "Tap to select a image",
-          pickImage: true,
-          labelVideo: "Image",
-          videoPath: videoPath,
-        ),
-      ],
+    return CardVideo(
+      hintText: "Tap to select a video",
+      labelVideo: "Video",
+      getVideoPath: (pathVideo) {
+        setState(() {
+          videoPath = pathVideo;
+        });
+      },
     );
-  }
-}
-
-class CardImageThumbnail extends StatefulWidget {
-  final String hintText;
-  CardImageThumbnail(
-      {required this.hintText,
-      required this.pickImage,
-      required this.labelVideo,
-      this.videoPath});
-  File? selectedImage;
-  final String labelVideo;
-  String? videoPath;
-
-  final bool pickImage;
-  @override
-  State<CardImageThumbnail> createState() => _CardImageThumbnailState();
-}
-
-class _CardImageThumbnailState extends State<CardImageThumbnail> {
-  Future<void> pickImage() async {
-    final ImagePicker _picker = ImagePicker();
-    // Pick an image from the gallery
-    XFile? video = await _picker.pickImage(source: ImageSource.gallery);
-
-    if (video != null) {
-      setState(() {
-        widget.selectedImage = File(video.path);
-      });
-    } else {
-      // User canceled the selection
-      print('No image selected');
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    print("pick Image bool ${widget.pickImage}");
-    return Column(
-      children: [
-        Card(
-          color: ListColor.colorBackgroundVideoContainer,
-          shape: BorderApp.border,
-          child: Container(
-            width: 110.w,
-            height: 150.h,
-            child: widget.selectedImage == null
-                ? GestureDetector(
-                    onTap: pickImage,
-                    child: Container(
-                      child: Center(
-                        child: ComponentTextDescription(
-                          "${widget.hintText}",
-                          fontSize: size.sizeTextDescriptionGlobal.sp,
-                          textAlign: TextAlign.center,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ),
-                  )
-                : Stack(
-                    children: [
-                      ClipRRect(
-                        borderRadius: BorderRadius.circular(30.r),
-                        child: widget.videoPath != null
-                            ? FutureBuilder<Uint8List?>(
-                                future: generateThumbnail(
-                                    widget.videoPath.toString()),
-                                builder: (context, snapshot) {
-                                  if (snapshot.connectionState ==
-                                          ConnectionState.done &&
-                                      snapshot.hasData) {
-                                    return Image.memory(
-                                      fit: BoxFit.cover,
-                                      width: double.infinity,
-                                      height: double.infinity,
-                                      snapshot.data!,
-                                    );
-                                  } else {
-                                    return CircularProgressIndicator();
-                                  }
-                                },
-                              )
-                            : Image.file(
-                                widget.selectedImage!,
-                                fit: BoxFit.cover,
-                                width: double.infinity,
-                                height: double.infinity,
-                              ),
-                      ),
-                      Align(
-                        alignment: Alignment.center,
-                        child: widget.pickImage == false
-                            ? Icon(
-                                Icons.play_circle_fill,
-                                size: 40,
-                                color: Colors.white,
-                              )
-                            : Icon(
-                                Icons.play_circle_fill,
-                                size: 0,
-                                color: Colors.white,
-                              ),
-                      ),
-                    ],
-                  ),
-          ),
-        ),
-        ComponentTextDescription("${widget.labelVideo}",
-            fontSize: size.sizeTextDescriptionGlobal)
-      ],
-    );
-  }
-
-  Future<Uint8List?> generateThumbnail(String videoPath) async {
-    final thumbnailBytes = await VideoThumbnail.thumbnailData(
-      video: videoPath,
-      imageFormat: ImageFormat.JPEG,
-      quality: 85,
-    );
-    return thumbnailBytes;
   }
 }
 
@@ -680,6 +545,9 @@ class CardVideo extends StatefulWidget {
       required this.labelVideo,
       required this.getVideoPath});
   File? selectedImage;
+  File? selectedImageThumbnail;
+  Uint8List? imageFrameVideo;
+
   final String labelVideo;
   final Function(String pathVideo) getVideoPath;
 
@@ -697,8 +565,23 @@ class _CardVideoState extends State<CardVideo> {
       setState(() {
         widget.selectedImage = File(video.path);
         print("Video path ${video.path}");
+        widget.selectedImageThumbnail = widget.selectedImage;
         //     widget.getVideoPath(video.path);
-        
+      });
+    } else {
+      // User canceled the selection
+      print('No image selected');
+    }
+  }
+
+  Future<void> pickImage() async {
+    final ImagePicker _picker = ImagePicker();
+    // Pick an image from the gallery
+    XFile? video = await _picker.pickImage(source: ImageSource.gallery);
+
+    if (video != null) {
+      setState(() {
+        widget.selectedImageThumbnail = File(video.path);
       });
     } else {
       // User canceled the selection
@@ -710,63 +593,141 @@ class _CardVideoState extends State<CardVideo> {
   Widget build(BuildContext context) {
     // TODO: implement build
 
-    return Column(
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
       children: [
-        Card(
-          color: ListColor.colorBackgroundVideoContainer,
-          shape: BorderApp.border,
-          child: Container(
-            width: 110.w,
-            height: 150.h,
-            child: widget.selectedImage == null
-                ? GestureDetector(
-                    onTap: pickVIdeo,
-                    child: Container(
-                      child: Center(
-                        child: ComponentTextDescription(
-                          "${widget.hintText}",
-                          fontSize: size.sizeTextDescriptionGlobal.sp,
-                          textAlign: TextAlign.center,
-                          fontWeight: FontWeight.bold,
+        Column(
+          children: [
+            Card(
+              color: ListColor.colorBackgroundVideoContainer,
+              shape: BorderApp.border,
+              child: Container(
+                width: 110.w,
+                height: 150.h,
+                child: widget.selectedImage == null
+                    ? GestureDetector(
+                        onTap: pickVIdeo,
+                        child: Container(
+                          child: Center(
+                            child: ComponentTextDescription(
+                              "${widget.hintText}",
+                              fontSize: size.sizeTextDescriptionGlobal.sp,
+                              textAlign: TextAlign.center,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
                         ),
+                      )
+                    : Stack(
+                        children: [
+                          ClipRRect(
+                              borderRadius: BorderRadius.circular(30.r),
+                              child: FutureBuilder<Uint8List?>(
+                                future: generateThumbnail(
+                                    widget.selectedImage!.path),
+                                builder: (context, snapshot) {
+                                  if (snapshot.connectionState ==
+                                          ConnectionState.done &&
+                                      snapshot.hasData) {
+                                    return Image.memory(
+                                      fit: BoxFit.cover,
+                                      width: double.infinity,
+                                      height: double.infinity,
+                                      snapshot.data!,
+                                    );
+                                  } else {
+                                    return CircularProgressIndicator();
+                                  }
+                                },
+                              )),
+                          Align(
+                              alignment: Alignment.center,
+                              child: Icon(
+                                Icons.play_circle_fill,
+                                size: 40,
+                                color: Colors.white,
+                              )),
+                        ],
                       ),
-                    ),
-                  )
-                : Stack(
-                    children: [
-                      ClipRRect(
-                          borderRadius: BorderRadius.circular(30.r),
-                          child: FutureBuilder<Uint8List?>(
-                            future:
-                                generateThumbnail(widget.selectedImage!.path),
-                            builder: (context, snapshot) {
-                              if (snapshot.connectionState ==
-                                      ConnectionState.done &&
-                                  snapshot.hasData) {
-                                return Image.memory(
-                                  fit: BoxFit.cover,
-                                  width: double.infinity,
-                                  height: double.infinity,
-                                  snapshot.data!,
-                                );
-                              } else {
-                                return CircularProgressIndicator();
-                              }
-                            },
-                          )),
-                      Align(
-                          alignment: Alignment.center,
-                          child: Icon(
-                            Icons.play_circle_fill,
-                            size: 40,
-                            color: Colors.white,
-                          )),
-                    ],
-                  ),
-          ),
+              ),
+            ),
+            ComponentTextDescription("${widget.labelVideo}",
+                fontSize: size.sizeTextDescriptionGlobal)
+          ],
         ),
-        ComponentTextDescription("${widget.labelVideo}",
-            fontSize: size.sizeTextDescriptionGlobal)
+        Column(
+          children: [
+            Card(
+              color: ListColor.colorBackgroundVideoContainer,
+              shape: BorderApp.border,
+              child: Container(
+                width: 110.w,
+                height: 150.h,
+                child: widget.selectedImageThumbnail == null
+                    ? GestureDetector(
+                        onTap: pickImage,
+                        child: Container(
+                          child: Center(
+                            child: ComponentTextDescription(
+                              "${widget.hintText}",
+                              fontSize: size.sizeTextDescriptionGlobal.sp,
+                              textAlign: TextAlign.center,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      )
+                    : Stack(
+                        children: [
+                          ClipRRect(
+                              borderRadius: BorderRadius.circular(30.r),
+                              child: FutureBuilder<Uint8List?>(
+                                future: generateThumbnail(
+                                    widget.selectedImageThumbnail!.path),
+                                builder: (context, snapshot) {
+                                  if (snapshot.connectionState ==
+                                          ConnectionState.done &&
+                                      snapshot.hasData) {
+                                    return Image.memory(
+                                      fit: BoxFit.cover,
+                                      width: double.infinity,
+                                      height: double.infinity,
+                                      snapshot.data!,
+                                    );
+                                  } else {
+                                    return Image.file(
+                                      widget.selectedImageThumbnail!,
+                                      fit: BoxFit.cover,
+                                      width: double.infinity,
+                                      height: double.infinity,
+                                    );
+                                  }
+                                },
+                              )),
+                          Align(
+                              alignment: Alignment.center,
+                              child: GestureDetector(
+                                onTap: pickImage,
+                                child: Container(
+                                  child: Center(
+                                    child: ComponentTextDescription(
+                                      "${widget.hintText}",
+                                      fontSize:
+                                          size.sizeTextDescriptionGlobal.sp,
+                                      textAlign: TextAlign.center,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                ),
+                              )),
+                        ],
+                      ),
+              ),
+            ),
+            ComponentTextDescription("${widget.labelVideo}",
+                fontSize: size.sizeTextDescriptionGlobal)
+          ],
+        )
       ],
     );
   }
@@ -778,6 +739,7 @@ class _CardVideoState extends State<CardVideo> {
       imageFormat: ImageFormat.JPEG,
       quality: 85,
     );
+    widget.imageFrameVideo = thumbnailBytes;
     return thumbnailBytes;
   }
 }
