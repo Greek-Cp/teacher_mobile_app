14c44697f97fe23a3803ff1c8e73b8022ff8a175
Greek-Cp <82995911+Greek-Cp@users.noreply.github.com>
2023-08-22 12:13:06 +0700

    Add new page add video category

---
+++ lib/view/page/profile/item_nav/profile_video/page_profile_add_video_category.dart
@@ -0,0 +1,338 @@
+import 'dart:ui';
+
+import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:lottie/lottie.dart';
+import 'package:teacher_mobile_app/res/colors/list_color.dart';
+import 'package:teacher_mobile_app/res/dimension/size.dart';
+import 'package:teacher_mobile_app/view/component/appbar/app_bar.dart';
+import 'package:teacher_mobile_app/view/component/button/button_arrow_back.dart';
+import 'package:teacher_mobile_app/view/component/button/button_long.dart';
+import 'package:teacher_mobile_app/view/component/button/text_description.dart';
+import 'package:teacher_mobile_app/view/page/profile/page_dashboard_profile.dart';
+
+class PageProfileAddVideoCategory extends StatefulWidget {
+  static String? routeName = "/PageAddVideoCategory";
+  @override
+  State<PageProfileAddVideoCategory> createState() =>
+      _PageProfileAddVideoCategoryState();
+}
+
+class _PageProfileAddVideoCategoryState
+    extends State<PageProfileAddVideoCategory> {
+  ScrollController? _scrollController;
+  bool lastStatus = true;
+  double height = 200;
+
+  void _scrollListener() {
+    if (_isShrink != lastStatus) {
+      setState(() {
+        lastStatus = _isShrink;
+      });
+    }
+  }
+
+  bool get _isShrink {
+    return _scrollController != null &&
+        _scrollController!.hasClients &&
+        _scrollController!.offset > (height - kToolbarHeight + 30.h);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _scrollController = ScrollController()..addListener(_scrollListener);
+  }
+
+  @override
+  void dispose() {
+    _scrollController?.removeListener(_scrollListener);
+    _scrollController?.dispose();
+    super.dispose();
+  }
+
+  bool showFollowing = true;
+  @override
+  Widget build(BuildContext context) {
+    final TextTheme textTheme = Theme.of(context).textTheme;
+
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Color.fromARGB(255, 14, 152, 184), // #08F4F9
+              Color.fromARGB(255, 101, 1, 155), // #B988FF
+            ],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+          ),
+        ),
+        child: NestedScrollView(
+          controller: _scrollController,
+          headerSliverBuilder: (context, innerBoxIsScrolled) {
+            return [];
+          },
+          body: CustomScrollView(
+            slivers: [
+              SliverPersistentHeader(
+                delegate: MySliverAppBarDelegate(),
+                pinned: true,
+              ),
+              SliverList(
+                delegate: SliverChildBuilderDelegate(
+                  (BuildContext context, int index) {
+                    return ListTile(title: Text('Item $index'));
+                  },
+                  childCount: 50,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class WidgetFollowing extends StatelessWidget {
+  const WidgetFollowing({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.only(
+          right: 20.w, left: size.sizePaddingLeftAndRightPage.w),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        mainAxisSize: MainAxisSize.max, // Ensures minimum required space
+        children: [
+          Container(
+            height: 60.h,
+            width: 60.w,
+            decoration:
+                BoxDecoration(color: Colors.white, shape: BoxShape.circle),
+          ),
+          SizedBox(
+            width: 10.w,
+          ),
+          Expanded(
+            child: ComponentTextDescription("Username",
+                teksColor: Colors.white,
+                fontSize: size.sizeTextDescriptionGlobal.sp),
+          ),
+          Card(
+            color: ListColor.colorButtonUnfollow,
+            child: Padding(
+              padding: EdgeInsets.symmetric(vertical: 10.h, horizontal: 15.w),
+              child: ComponentTextDescription("Unfollow",
+                  fontSize: size.sizeTextDescriptionGlobal.sp),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class CardItemPageProfileAddVideoCategory extends StatelessWidget {
+  Color? colorCardItem;
+  String? valueCard;
+  String? descriptionCard;
+  Color? colorFont;
+  CardItemPageProfileAddVideoCategory(
+      {super.key,
+      required this.colorCardItem,
+      required this.valueCard,
+      required this.descriptionCard,
+      this.colorFont});
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Card(
+        margin: EdgeInsets.symmetric(horizontal: 10.w),
+        color: colorCardItem,
+        shape: RoundedRectangleBorder(
+          side: BorderSide(
+            width: size.sizeBorderBlackGlobal,
+          ),
+          borderRadius: BorderRadius.circular(size.sizeRoundedGlobal),
+        ),
+        child: Container(
+          height: 60.h,
+          width: 100.w,
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              ComponentTextDescription(
+                "${valueCard}",
+                fontWeight: FontWeight.bold,
+                fontSize: size.sizeTextHeaderGlobal - 2.sp,
+                teksColor:
+                    colorFont == null ? ListColor.colorFontPageNav : colorFont!,
+              ),
+              ComponentTextDescription(
+                "$descriptionCard",
+                fontWeight: FontWeight.bold,
+                fontSize: size.sizeTextDescriptionGlobal - 5.sp,
+                teksColor:
+                    colorFont == null ? ListColor.colorFontPageNav : colorFont!,
+                textAlign: TextAlign.center,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class MySliverAppBarDelegate extends SliverPersistentHeaderDelegate {
+  @override
+  double get minExtent => kToolbarHeight * 1.5;
+
+  @override
+  double get maxExtent => 110.0;
+
+  @override
+  Widget build(
+      BuildContext context, double shrinkOffset, bool overlapsContent) {
+    bool _isShrink = shrinkOffset > 0;
+
+    return Padding(
+      padding: EdgeInsets.only(left: 0, right: 0, top: 0, bottom: 0),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          ButtonBackArrow(
+            onPressed: () {},
+          ),
+          Expanded(
+            child: Container(
+              decoration: BoxDecoration(
+                  border: Border.all(color: Colors.black, width: 1.0),
+                  color: Colors.grey,
+                  borderRadius: BorderRadius.all(Radius.circular(20))),
+              child: Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Text(
+                  "new_video",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                    fontSize: 16.0,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+            ),
+          ),
+          GestureDetector(
+            onTap: () {
+              Navigator.of(context).pop();
+            },
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 15.0,
+                ),
+                Transform.rotate(
+                  angle: 180 * (3.141592653589793238 / 180),
+                  child: IconButton(
+                    icon: Icon(Icons.arrow_back),
+                    onPressed: () {},
+                  ),
+                ),
+                Text(
+                  "Next",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
+                    fontSize: 14.0,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  bool shouldRebuild(covariant SliverPersistentHeaderDelegate oldDelegate) {
+    return true;
+  }
+}
+
+class TranslucentSliverAppBar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SliverPersistentHeader(
+        floating: true,
+        pinned: true,
+        delegate: _TranslucentSliverAppBarDelegate(
+          MediaQuery.of(context).padding,
+        ));
+  }
+}
+
+class _TranslucentSliverAppBarDelegate extends SliverPersistentHeaderDelegate {
+  /// This is required to calculate the height of the bar
+  final EdgeInsets safeAreaPadding;
+
+  _TranslucentSliverAppBarDelegate(this.safeAreaPadding);
+
+  @override
+  double get minExtent => safeAreaPadding.top;
+
+  @override
+  double get maxExtent => minExtent + kToolbarHeight;
+
+  @override
+  Widget build(
+      BuildContext context, double shrinkOffset, bool overlapsContent) {
+    return ClipRect(
+        child: BackdropFilter(
+            filter: ImageFilter.blur(sigmaX: 16, sigmaY: 16),
+            child: Opacity(
+                opacity: 0.93,
+                child: Container(
+                    // Don't wrap this in any SafeArea widgets, use padding instead
+                    padding: EdgeInsets.only(top: safeAreaPadding.top),
+                    height: maxExtent,
+                    color: Colors.white,
+                    // Use Stack and Positioned to create the toolbar slide up effect when scrolled up
+                    child: Stack(
+                      children: <Widget>[
+                        Positioned(
+                          bottom: 0,
+                          left: 0,
+                          right: 0,
+                          child: AppBar(
+                            primary: false,
+                            elevation: 0,
+                            backgroundColor: Colors.transparent,
+                            title: Text("Translucent App Bar"),
+                          ),
+                        )
+                      ],
+                    )))));
+  }
+
+  @override
+  bool shouldRebuild(_TranslucentSliverAppBarDelegate old) {
+    return maxExtent != old.maxExtent ||
+        minExtent != old.minExtent ||
+        safeAreaPadding != old.safeAreaPadding;
+  }
+}
