db703cf36ee5060a9a6edd84c3eb25baf5738bca
Yanuar Tr <82995911+Greek-Cp@users.noreply.github.com>
2023-08-28 23:46:48 +0700

    Update Page Profile SElect  Language

--- lib/view/page/profile/item_nav/profile_menu/page_profile_menu_select_language.dart
+++ lib/view/page/profile/item_nav/profile_menu/page_profile_menu_select_language.dart
@@ -109,7 +109,7 @@ class _PageProfileMenuSelectLanguageState
     if (lang == "AR") {
       print("arab");
     } else {
-      print("englis");
+      print("engliss");
     }
 
     return Scaffold(
