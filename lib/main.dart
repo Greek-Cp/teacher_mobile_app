35b9d1b3bddd531f6f8e0ebb0652608a787f6ef8
Yanuar Tr <82995911+Greek-Cp@users.noreply.github.com>
2023-09-01 17:08:04 +0700

    Change Route to back

--- lib/main.dart
+++ lib/main.dart
@@ -83,7 +83,7 @@ class MainApp extends StatelessWidget {
           supportedLocales: context.supportedLocales,
           locale: context.locale,
           debugShowCheckedModeBanner: false,
-          initialRoute: PageProfileUploadVideo.routeName.toString(),
+          initialRoute: PageSelectLanguage.routeName.toString(),
           defaultTransition: Transition.circularReveal,
           getPages: [
             GetPage(
