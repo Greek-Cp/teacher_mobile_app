eab8cb3d8bfdd07d16e35ca7d68a0ece12ada127
Yanuar Tr <82995911+Greek-Cp@users.noreply.github.com>
2023-09-06 14:18:28 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -85,7 +85,7 @@ class MainApp extends StatelessWidget {
           supportedLocales: context.supportedLocales,
           locale: context.locale,
           debugShowCheckedModeBanner: false,
-          initialRoute: PageSelectLanguage.routeName.toString(),
+          initialRoute: PageProfileAddVideoMenu.routeName.toString(),
           defaultTransition: Transition.circularReveal,
           getPages: [
             GetPage(
