03974c3a398185613238cf534873eee3b0a1efe0
Yanuar Tr <82995911+Greek-Cp@users.noreply.github.com>
2023-09-20 22:39:22 +0700

    Update Page Profile Add Video Course

---
+++ lib/view/page/profile/item_nav/profile_video/page_profile_add_video_course.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class PageProfileAddVideoCourse extends StatefulWidget {
+  @override
+  State<PageProfileAddVideoCourse> createState() =>
+      _PageProfileAddVideoCourseState();
+}
+
+class _PageProfileAddVideoCourseState extends State<PageProfileAddVideoCourse> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      body: Column(children: []),
+    );
+  }
+}
